d023f242a9b4581a4efec1f57998795777670c11
Applepiea <vapel.rosenberg@gmail.com>
2025-11-05 21:29:05 +0100

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    image: '/images/event1.png',
+    title: 'React Summit 2025',
+    slug: 'react-summit-2025',
+    location: 'Amsterdam, Netherlands',
+    date: 'June 2-3, 2025',
+    time: '9:00 AM - 6:00 PM'
+  },
+  {
+    image: '/images/event2.png',
+    title: 'JavaScript Conference 2025',
+    slug: 'js-conf-2025',
+    location: 'San Francisco, CA',
+    date: 'April 8-10, 2025',
+    time: '8:30 AM - 5:30 PM'
+  },
+  {
+    image: '/images/event3.png',
+    title: 'Web Dev Hackathon',
+    slug: 'web-dev-hackathon-2025',
+    location: 'New York, NY',
+    date: 'March 15-17, 2025',
+    time: '10:00 AM - 10:00 PM'
+  },
+  {
+    image: '/images/event4.png',
+    title: 'Tech Meetup: Frontend Masters',
+    slug: 'frontend-masters-meetup',
+    location: 'Seattle, WA',
+    date: 'February 20, 2025',
+    time: '6:00 PM - 8:00 PM'
+  },
+  {
+    image: '/images/event5.png',
+    title: 'DevOps Conference 2025',
+    slug: 'devops-conf-2025',
+    location: 'London, UK',
+    date: 'May 12-14, 2025',
+    time: '9:00 AM - 5:00 PM'
+  },
+  {
+    image: '/images/event6.png',
+    title: 'Node.js World Conference',
+    slug: 'nodejs-world-2025',
+    location: 'Toronto, Canada',
+    date: 'July 21-23, 2025',
+    time: '8:00 AM - 6:00 PM'
+  }
+]
